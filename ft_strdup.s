0175bd9068c7febf2f35d03073471d0da4939f7b
ElHuaco <alexhuac@gmail.com>
2020-08-05 04:30:27 -0700

    Comentarios dup

--- ft_strdup.s
+++ ft_strdup.s
@@ -10,8 +10,6 @@
 ;                                                                              ;
 ; **************************************************************************** ;
 ; 1st function parameter: rdi -> s1
-; local counter: rcx -> i
-; local variable pointer: rsi -> dup
 ;label			instruction			operands		comment
 			section				.text
 			global				_ft_strdup
@@ -20,8 +18,8 @@
 			extern				_malloc
 _ft_strdup:
 			push				rdi			;Saving rdi before function call.
-			cmp				rdi, 0
-			je				_null_return		;If s1 (null), return 0.
+			cmp				rdi, 0			;If s1 is (null), restore rdi
+			je				_null_return		; and return 0.
 			call				_ft_strlen		;Now we have strlen(s1) in rax.
 			pop				rdi			;Restoring rdi.
 			inc				rax			;Need to malloc len+1 for the '\0'.
@@ -34,8 +32,8 @@ _ft_strdup:
 			push				rsi			;Saving rsi before ft_strcpy call.
 			mov				rsi, rdi		;src is 2nd param in ft_strcpy
 			mov				rdi, rax		;dst is 1st param in ft_strcpy
-			call				_ft_strcpy		;ft_strcpy(rdi=dup, rsi=s1); Now dup in rax.
-			pop				rsi
+			call				_ft_strcpy		;ft_strcpy(rdi=dup, rsi=s1). Now dup in rax.
+			pop				rsi			;Restore rsi.
 			ret
 _null_return:
 			pop				rdi
